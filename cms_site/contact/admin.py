2f70dc84227f6e455bf71b04c967ed4485179a74
Sebastian Felipe Araque Perez <sebaspipe18@gmail.com>
2025-09-30 15:03:10 -0500

    Agregando preguntas frecuentes

--- cms_site/contact/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import ContactMessage
-
-@admin.register(ContactMessage)
-class ContactMessageAdmin(admin.ModelAdmin):
-    list_display = ("created_at", "name", "email", "subject", "terms")
-    list_filter = ("created_at", "terms")
-    search_fields = ("name", "email", "subject", "message")
-    readonly_fields = ("created_at", "ip", "user_agent")
--- cms_site/contact/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import inbox
-
-urlpatterns = [
-    path("inbox/", inbox, name="contact_inbox"),
-]
